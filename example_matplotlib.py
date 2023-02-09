93aef5bdba8def67e2084fd6dd63b5445749e442
David Stansby <dstansby@gmail.com>
2023-02-09 20:37:18 +0000

    Clean example

--- example_matplotlib.py
+++ example_matplotlib.py
@@ -3,48 +3,51 @@ sunpy CI jobs
 =============
 """
 import os
+from datetime import date
 
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import pint
 
-from ci_impact.emissions import power_usage
+from ci_impact.emissions import emissions
 from ci_impact.gh import GhApi, load_cached_job_info
 
 u = pint.UnitRegistry()
 
-org = "sunpy"
-repo = "sunpy"
+org = "matplotlib"
+repo = "matplotlib"
 
 ##################################################
 # Load data
-api = GhApi(token=os.environ["GHAPI_TOKEN"])
+scrape_api = False
 
-# df = api.get_job_runtimes(org=org, repo=repo, start_date=date(2022, 6, 18))
+start_date = date(2023, 1, 1)
+if scrape_api:
+    api = GhApi(token=os.environ["GHAPI_TOKEN"])
+    df = api.get_job_runtimes(org=org, repo=repo, start_date=start_date)
+
 df = load_cached_job_info(org=org, repo=repo)
+df = df[df["started_at"] > pd.to_datetime(start_date, utc=True)]
+df = df[df["started_at"] < pd.to_datetime(date(2023, 2, 1), utc=True)]
 # Convert times to hours
-df["power_usage"] = power_usage(df["os"].values, df["running_time"].values).m_as(
-    u.kW * u.h
+df["emissions"] = (
+    emissions(df["os"].values, df["running_time"].values).to(u.kg).magnitude
 )
 df["running_time"] = df["running_time"] / np.timedelta64(1, "h")
 
 ##################################################
 # Plot data
-fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(5, 8))
-fig.subplots_adjust(bottom=0.33)
+fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(5, 8), constrained_layout=True)
+
 # Plot total
 ax = axs[0]
-ax.plot(df["started_at"], np.cumsum(df["running_time"]), color="k")
-
-ax.set_title(f"Total CI time for {org}/{repo}")
+ax.set_title(f"Estimated CO2 emissions from GitHub Actions\n{org}/{repo}")
+ax.plot(df["started_at"], np.cumsum(df["emissions"]), color="tab:blue")
 ax.set_ylim(0)
-ax.set_ylabel("Hours")
-
-ax = ax.twinx()
-ax.plot(df["started_at"], np.cumsum(df["power_usage"]), color="tab:blue")
-ax.set_ylim(0)
-ax.set_ylabel("kWH")
+ax.set_ylabel("kgeCO2")
+ax.margins(x=0)
 
 ax = axs[1]
 times = {}
@@ -55,15 +58,21 @@ for name in np.unique(df["name"]):
 
 all_ts = np.sort([t.iloc[-1] for t in times.values()])
 
+other_t = None
+n_jobs = 5
 for name, t in times.items():
-    if t.iloc[-1] < all_ts[-10]:
+    other_t = t if other_t is None else other_t + t
+    if t.iloc[-1] < all_ts[-n_jobs]:
         continue
 
     label = name.split("/")[1][1:] if "/" in name else name
     ax.plot(df["started_at"], t, label=label)
 
 ax.legend(bbox_to_anchor=(0.1, -0.2), loc="upper left", fontsize=8)
-ax.set_title("Top 10 jobs")
+ax.set_title(f"Top {n_jobs} jobs")
+ax.set_ylabel("Hours")
+ax.set_ylim(0)
+ax.margins(x=0)
 
 locator = mdates.AutoDateLocator()
 formatter = mdates.ConciseDateFormatter(locator)
